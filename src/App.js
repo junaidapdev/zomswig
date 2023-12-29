b14beb8fb4299cd725d7fe5788c95d0006f27f41
junaidapdev <junaidap.dev@gmail.com>
2023-12-29 08:11:39 +0530

    dynamic restaurant cards

--- src/App.js
+++ src/App.js
@@ -37,27 +37,55 @@ const RestaurantCard = (props) => {
  
   const {resData} = props
 
+  const {name,location, cuisine, rating } = resData;
+
   return(
     <div className="res-card">
       <img className='res-logo' src="https://img.freepik.com/free-photo/fresh-pasta-with-hearty-bolognese-parmesan-cheese-generated-by-ai_188544-9469.jpg" alt="" />
-      <h3></h3>
-      <h4>Biriyani, North indian, Chinese</h4>
-      <h4>4.4 Stars</h4>
-      <h4>33 Minutes</h4>
+      <h3>{name}</h3>
+      <h4>{location}</h4>
+      <h4>{cuisine}</h4>
+      <h4>{rating}</h4>
     </div>
   )
 }
+
+const resList = [
+  {
+    "id": 1,
+    "name": "The Gourmet Kitchen",
+    "location": "123 Foodie Street, Flavor Town",
+    "cuisine": "Italian, Indian, Mangolian",
+    "rating": 4.5
+  },
+  {
+    "id": 2,
+    "name": "kannur Kitchen",
+    "location": "123 Foodie Street, Flavor Town",
+    "cuisine": "Italian, Indian, Mangolian",
+    "rating": 4.5
+  },
+  {
+    "id": 3,
+    "name": "Kerala Kitchen",
+    "location": "123 Foodie Street, Flavor Town",
+    "cuisine": "Italian, Indian, Mangolian",
+    "rating": 4.5
+  }
+]
+
+
 
 const Body = () => {
   return(
     <div className="body">
       <div className="search">Search</div>
       <div className="resContainer">
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        {
+          resList.map(restaurant => (
+            <RestaurantCard key={restaurant.id} resData={restaurant}/> 
+          ))
+        }
       </div>
     </div>
   )
