fbde1176c1acabea62dc81be438f0acf33253bdb
junaidapdev <junaidap.dev@gmail.com>
2024-01-01 23:30:24 +0530

    improved navbar design

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,3 @@
 export const foodImageUrl = "https://img.freepik.com/free-photo/fresh-pasta-with-hearty-bolognese-parmesan-cheese-generated-by-ai_188544-9469.jpg"
-export const logo = 'https://assets.materialup.com/uploads/08467c74-4744-4eac-817f-598b2f2fd680/preview.png'
+export const logo = 'https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148987940.jpg?w=1380&t=st=1704131295~exp=1704131895~hmac=0ee373f1de6132a2c72f3a53a8e25b244f7527c34b08a209573387e7c5c4636f'
 
